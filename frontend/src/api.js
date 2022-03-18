f219fdbead726bc4d8dea39d7752eed0086b63d3
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2022-03-18 22:27:43 +0530

    Alert Messages & added  JWT token to requests

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,9 +1,25 @@
 import axios from "axios";
+import { logout } from "./shared/utils/auth";
 
 const apiClient = axios.create({
 	baseURL: "http://localhost:5002/api",
 	timeout: 1000,
 });
+
+apiClient.interceptors.request.use(
+	(config) => {
+		const userDetails = localStorage.getItem("user");
+
+		if (userDetails) {
+			const token = JSON.parse(userDetails).token;
+			config.headers.Authorization = `Bearer ${token}`;
+		}
+		return config;
+	},
+	(err) => {
+		return Promise.reject(err);
+	}
+);
 
 export const login = async (data) => {
 	try {
@@ -18,5 +34,14 @@ export const register = async (data) => {
 		return await apiClient.post("/auth/register", data);
 	} catch (exception) {
 		return { error: true, exception };
+	}
+};
+
+//secure routes
+
+const checkResponseCode = (exception) => {
+	const responseCode = exception?.response?.statuslogout;
+	if (responseCode) {
+		(responseCode === 401 || responseCode === 403) && logout();
 	}
 };
