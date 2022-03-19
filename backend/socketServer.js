e1abac335cea31f39b652ebd0e1f5fd0c698c16b
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2022-03-19 12:37:17 +0530

    Saving info about connected users

--- backend/socketServer.js
+++ backend/socketServer.js
@@ -1,4 +1,5 @@
 const authSocket = require("./middleware/authSocket");
+const newConnectionHandler = require("./socketHandlers/newConnectionHandler");
 
 const registerSocketServer = (server) => {
 	const io = require("socket.io")(server, {
@@ -15,8 +16,7 @@ const registerSocketServer = (server) => {
 	io.on("connection", (socket) => {
 		console.log("User connected");
 		console.log(socket.id);
-
-		// new connection handler
+		newConnectionHandler(socket, io);
 	});
 };
 
