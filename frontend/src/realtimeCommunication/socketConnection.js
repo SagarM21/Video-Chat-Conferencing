218b1f7bc58a908237af383f8842da8a20dd659d
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2024-01-18 17:44:08 +0530

    typo: fix

--- frontend/src/realtimeCommunication/socketConnection.js
+++ frontend/src/realtimeCommunication/socketConnection.js
@@ -13,9 +13,10 @@ let socket = null;
 
 export const connectWithSocketServer = (userDetails) => {
   const socketString =
-    process.env.REACT_APP_PRODUCTION === true
+    process.env.REACT_APP_PRODUCTION === "true"
       ? "https://rtc-video-backend.vercel.app"
       : "http://localhost:5002";
+//   console.log(socketString, "socketString");
   const jwtToken = userDetails.token;
   socket = io(socketString, {
     auth: {
