ab3572494769c5243fb1474bfe466c43651486ae
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2024-01-18 17:46:55 +0530

    resolve : cors

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,11 @@ const PORT = process.env.PORT || process.env.API_PORT;
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 //register routes
 app.use("/api/auth", authRoutes);
@@ -22,13 +26,13 @@ const server = http.createServer(app);
 socketServer.registerSocketServer(server);
 
 mongoose
-	.connect(process.env.MONGO_URI)
-	.then(() => {
-		server.listen(PORT, () => {
-			console.log(`Server is listening on ${PORT}`);
-		});
-	})
-	.catch((err) => {
-		console.log("Database connection failed. Server not started");
-		console.error(err);
-	});
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    server.listen(PORT, () => {
+      console.log(`Server is listening on ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Database connection failed. Server not started");
+    console.error(err);
+  });
