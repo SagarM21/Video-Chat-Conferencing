ef92c216dc844d28ed37a40cf830734d4c20eac7
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2024-01-18 18:01:35 +0530

    check

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://p2p-webtrc.vercel.app/",
+    origin: "https://rtc-video-backend.vercel.app/",
   })
 );
 
